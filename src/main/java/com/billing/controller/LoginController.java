0f72f179f085af3d140bc49b1e15859bf9428006
psantra <pijushsantra29@gmail.com>
2019-04-22 19:55:50 -0500

    Login Model Created

---
+++ src/main/java/com/billing/controller/LoginController.java
@@ -0,0 +1,37 @@
+package com.billing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.billing.model.LoginModel;
+
+@Controller
+public class LoginController {
+	@RequestMapping(value = "login", method = RequestMethod.GET)
+	public String init(Model model) {
+		System.out.println("Inside Get Method");
+		model.addAttribute("msg", "Please Enter Your Login Details");
+		return "login.jsp";
+	}
+
+	@RequestMapping(value = "loginValidate", method = RequestMethod.POST)
+	public String submit(Model model, @ModelAttribute("loginBean") LoginModel loginModel) {
+
+		System.out.println("Inside post method.....");
+		if (loginModel != null && loginModel.getUserName() != null & loginModel.getPassword() != null) {
+			if (loginModel.getUserName().equals("admin") && loginModel.getPassword().equals("admin")) {
+				model.addAttribute("msg", loginModel.getUserName());
+				return "success.jsp";
+			} else {
+				model.addAttribute("error", "Invalid Details");
+				return "login.jsp";
+			}
+		} else {
+			model.addAttribute("error", "Please enter Details");
+			return "login.jsp";
+		}
+	}
+}
